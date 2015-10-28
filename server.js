798866bc6f276160460c0c16a48cbb2ce92d25a5
Miika Hänninen <miika.hanninen@gmail.com>
2015-10-28 10:55:15 +0200

    Add a directory listing to debug heroku

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
 'use strict';
 
 import express from 'express';
+import { readdir } from 'fs';
 
 const app = express();
+const docPath = __dirname + '/doc';
 
 app.get('/', function (req, res) {
-  res.send('Drinkkiarkisto.');
+  readdir(docPath, function(err, files) {
+    let result = 'Drinkkiarkisto. ' + docPath + '\n';
+    files.forEach(function(file) {
+      result += file + '\n';
+    });
+
+    res.send(result);
+  });
 });
 
 app.use('/doc', express.static(__dirname + '/doc'));
