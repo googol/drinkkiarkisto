778557f216ba4c4d1506c3964f4652f8e9eadef1
Miika Hänninen <miika.hanninen@gmail.com>
2015-10-28 13:21:15 +0200

    Create static index page

--- server.js
+++ server.js
@@ -5,10 +5,6 @@ import express from 'express';
 const app = express();
 const publicPath = __dirname + '/public';
 
-app.get('/', function (req, res) {
-  res.send('Drinkkiarkisto. Dokumentaatio: doc/documentation.pdf');
-});
-
 app.use('/', express.static(publicPath));
 
 export default app;
