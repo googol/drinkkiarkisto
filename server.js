a749f8e1f93dfc925a029c9e2ef738545c4af3ee
Miika Hänninen <miika.hanninen@gmail.com>
2015-11-01 01:02:35 +0200

    Enable ejs template engine for page rendering

--- server.js
+++ server.js
@@ -3,6 +3,12 @@ import express from 'express';
 const app = express();
 const publicPath = __dirname + '/public';
 
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.get('/', function(req,res) {
+  res.render('index');
+});
 app.use('/', express.static(publicPath));
 
 export default app;
