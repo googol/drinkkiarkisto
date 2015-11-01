85a702483b0a4899d90157798c85c43e80868f41
Miika Hänninen <miika.hanninen@gmail.com>
2015-11-01 15:22:28 +0200

    Improve menu and add some page stubs

--- server.js
+++ server.js
@@ -6,9 +6,32 @@ const publicPath = __dirname + '/public';
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
-app.get('/', function(req,res) {
+app.get('/', function(req, res) {
   res.render('index');
 });
+
+app.get('/register', function(req, res) {
+  res.render('register');
+});
+
+app.get('/login', function(req, res) {
+  res.render('login');
+});
+app.post('/login', function(req, res) {
+  res.redirect('/');
+});
+
+app.get('/logout', function(req, res) {
+  res.render('logout');
+});
+app.post('/logout', function(req, res) {
+  res.redirect('/');
+});
+
+app.get('/profile', function(req, res) {
+  res.render('profile');
+});
+
 app.use('/', express.static(publicPath));
 
 export default app;
