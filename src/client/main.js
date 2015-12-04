063783dec6c8f75c2fc247a6a27fa659be8ef886
Miika Hänninen <miika.hanninen@gmail.com>
2015-12-04 21:46:14 +0200

    Render header with cyclejs

--- src/client/main.js
+++ src/client/main.js
@@ -1,11 +1,25 @@
 import Cycle from '@cycle/core';
 import CycleDOM from '@cycle/dom';
+import { Observable } from 'rx';
+import { renderHeader } from '../views';
 import makeExternalLinkDriver from './externalLinkDriver';
 
 function main({ DOM }) {
   const externalLinkClick$ = DOM.select('a[rel=external]').events('click');
 
+  const user = {
+    isAdmin: true,
+    email: 'admin@example.com',
+    id: 1,
+  };
+  const locals = {
+    user,
+    query: '',
+  };
+  const view = Observable.just(locals).concat(Observable.never()).map(locals => renderHeader(locals.user, locals.query));
+
   return {
+    DOM: view,
     externalLink: externalLinkClick$,
   };
 }
