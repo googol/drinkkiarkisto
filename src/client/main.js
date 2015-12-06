5ddde41d90cd06dfc06e9405b59755a2606d0154
Miika Hänninen <miika.hanninen@gmail.com>
2015-12-06 14:41:58 +0200

    Implement client side routing

--- src/client/main.js
+++ src/client/main.js
@@ -2,13 +2,78 @@ import 'babel-polyfill';
 import { run } from '@cycle/core';
 import { makeDOMDriver } from '@cycle/dom';
 import { makeHistoryDriver, filterLinks } from '@cycle/history';
+import { Observable } from 'rx';
 import switchPath from 'switch-path';
 import makeExternalLinkDriver from './externalLinkDriver';
 
 function main({ DOM, history }) {
+  const navigationIntent = navigationIntents(history);
+
+  const navigateTo$ = Observable.merge(
+    getInternalLinkClicks(DOM),
+    navigationIntent.redirectTo$.map(param => param.path));
+
   return {
     externalLink: getExternalLinkClicks(DOM),
-    history: getInternalLinkClicks(DOM),
+    history: navigateTo$,
+  };
+}
+
+function navigationIntents(history) {
+  function createRoute(routeId, routeParam) {
+    routeParam = routeParam || { };
+
+    return [routeId, routeParam];
+  }
+
+  function getRouteIntent(route$, routeId) {
+    const routeMatches = ({ value }) => value[0] === routeId;
+    const returnParam = ({ value }) => value[1];
+
+    return route$.filter(routeMatches).map(returnParam);
+  }
+
+  const routes = {
+    drinkList: 'drinkList',
+    drinkSingle: 'drink',
+    drinkEdit: 'drinkEdit',
+    drinkNew: 'drinkNew',
+    ingredientList: 'ingredientList',
+    drinkTypeList: 'drinkTypeList',
+    userList: 'userList',
+    registration: 'registration',
+    login: 'login',
+    profile: 'profile',
+    redirectTo: 'redirectTo',
+  };
+
+  const route$ = history
+    .map(location => switchPath(location.pathname, {
+      '/': createRoute(routes.drinkList),
+      '/drinks': {
+        '/': () => location.query.edit && createRoute(routes.drinkNew) || createRoute(routes.redirectTo, { path: '/' }),
+        '/:drinkId': drinkId => location.query.edit && createRoute(routes.drinkEdit, { drinkId }) || createRoute(routes.drinkSingle, { drinkId }),
+      },
+      '/ingredients': createRoute(routes.ingredientList),
+      '/drinkTypes': createRoute(routes.drinkTypeList),
+      '/users': createRoute(routes.userList),
+      '/register': createRoute(routes.registration),
+      '/login': createRoute(routes.login),
+      '/profile': createRoute(routes.profile),
+    }));
+
+  return {
+    drinkList$: getRouteIntent(route$, routes.drinkList),
+    drink$: getRouteIntent(route$, routes.drinkSingle),
+    drinkEdit$: getRouteIntent(route$, routes.drinkEdit),
+    drinkNew$: getRouteIntent(route$, routes.drinkNew),
+    ingredientList$: getRouteIntent(route$, routes.ingredientList),
+    drinkTypeList$: getRouteIntent(route$, routes.drinkTypeList),
+    userList$: getRouteIntent(route$, routes.userList),
+    registration$: getRouteIntent(route$, routes.registration),
+    login$: getRouteIntent(route$, routes.login),
+    profie$: getRouteIntent(route$, routes.profile),
+    redirectTo$: getRouteIntent(route$, routes.redirectTo),
   };
 }
 
