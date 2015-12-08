a633830d83e839bbf93c6391b5d44fbb5d2af371
Miika Hänninen <miika.hanninen@gmail.com>
2015-12-08 14:22:03 +0200

    Fix some weirdness in model.js

--- src/client/model.js
+++ src/client/model.js
@@ -1,10 +1,8 @@
-import { BehaviorSubject } from 'rx';
+import { Observable } from 'rx';
 
 export function getModel(navigationIntent, receive) {
   return {
-    drinkList$: navigationIntent.drinkList$
-      .map(() => receive.drinks.all())
-      .switch()
+    drinkList$: receive.drinks.all()
       .flatMap(response$ => response$)
       .map(response => response.body.drinks),
 
@@ -13,8 +11,14 @@ export function getModel(navigationIntent, receive) {
       .filter(response => response.body.successes || response.body.errors)
       .map(response => ({ successes: response.body.successes, errors: response.body.errors }))
       .startWith({ successes: [], errors: [] }),
-    query$: new BehaviorSubject(''),
-    user$: new BehaviorSubject(null),
+
+    query$: Observable.just(''),
+
+    user$: receive.users.profile()
+      .flatMap(response$ => response$)
+      .map(response => respose.body.user)
+      .startWith(null),
+
     redirectTo$: navigationIntent.redirectTo$.map(param => param.path),
   };
 }
